406e45e62b76014417c0858ed1ede5a59bef402e
Stefano Sotelo <stefano.sotelo@pucp.pe>
2021-06-09 16:13:05 +0200

    add final changes

--- src/components/Screens/TableOverview/TableOverview.js
+++ src/components/Screens/TableOverview/TableOverview.js
@@ -1,26 +1,56 @@
 import React, { Component } from "react";
 import TitleBar from "../../Bar/TitleBar/StandardTitleBar";
 import Table from "../../UI/Table/Ant/Table";
+import UpdateButton from "../../UI/Button/Standard/StandardButton";
 
 import styles from "./TableOverview.module.css";
 import {
   propsTitleBar
 } from "./utils/props";
-import _, { set } from "lodash";
+import _ from "lodash";
+import axiosCoatmasterFlex from "../../../api/axios";
 
 class TableOverview extends Component {
 
   state = {
-    report:{}
+    report: {},
+    threshold: {
+      lowerLimit: -1,
+      upperLimit: -1
+    }
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     this.setState({ report: localStorage.getItem("report") });
+
+    // Request threshold values
+    let response;
+
+    try {
+      response = await axiosCoatmasterFlex("/pphd_configuration/threshold");
+    } catch (error) {
+      console.error("[componentDidMount] Not possible to fecth threshold data");
+      return;
+    }
+
+    const threshold = response.data;
+
+    this.setState(prevState => {
+      const nextState = { ...prevState };
+      nextState.threshold = threshold;
+      return nextState;
+    });
   }
 
   componentWillUnmount() {
     console.log("[componentWillUnmount: TableOverview ");
     delete localStorage.report;
+  }
+
+  updateData() {
+   console.log("Update table data");
+   window.open("/coatmaster-flex/table", "_self");
+   window.close();
   }
 
   render() {
@@ -29,6 +59,9 @@ class TableOverview extends Component {
     // Data for report
     const dataForReport = this.state.report;
 
+    // Thresholds value
+    const { upperLimit, lowerLimit } = this.state.threshold;
+
     // CSS classes
     const {
       tableOverview,
@@ -36,7 +69,12 @@ class TableOverview extends Component {
       headerBox,
       main,
       titleBarBox,
-      tableBox
+      tableBox,
+      controlsBox,
+      labelBox,
+      labelText,
+      labelValue
+
     } = styles;
     return (
       <div className={tableOverview}>
@@ -48,9 +86,31 @@ class TableOverview extends Component {
           </div>
 
           <div className={main}>
-            <div className={tableBox}>
-              <Table report={dataForReport} />
+
+            <div className={controlsBox}>
+              <div className={labelBox}>
+                <span className={labelText}> Upper limit:</span>
+                <span className={labelValue} >{upperLimit}</span>
+              </div>
+
+              <div className={labelBox}>
+                <span className={labelText} >Lower limit:</span>
+                <span className={labelValue} >{lowerLimit}</span>
+              </div>
+
+              <div className={labelBox}>
+                <UpdateButton
+                  updater={this.updateData.bind(this,)}
+                >
+                  Update
+                </UpdateButton>
+              </div>
             </div>
+
+            <div className={tableBox}>
+              <Table report={dataForReport} threshold={{ upperLimit, lowerLimit }} />
+            </div>
+
           </div>
         </div>
       </div>
--- src/components/UI/Table/Ant/Table.js
+++ src/components/UI/Table/Ant/Table.js
@@ -2,6 +2,7 @@ import { Table } from 'antd';
 import "./Table.css"
 
 import { ReactComponent as Product } from "../../../../assets/svg/product.svg";
+import { ReactComponent as Application } from "../../../../assets/svg/documents.svg";
 import { ReactComponent as PeakHigh } from "../../../../assets/svg/peakHigh.svg";
 import { ReactComponent as PeakLow } from "../../../../assets/svg/peakLow.svg";
 import { ReactComponent as Runtime } from "../../../../assets/svg/runtime.svg";
@@ -9,7 +10,17 @@ import statusOk from "../../../../assets/icons/statusAllrightBig.ico";
 import statusWarning from "../../../../assets/icons/statusWarningBig.ico";
 import statusAlert from "../../../../assets/icons/statusAlertBig.ico";
 import { ReactComponent as Thickness } from "../../../../assets/svg/thickness.svg";
+
 import _ from 'lodash';
+import { toaster } from "evergreen-ui";
+
+const propsToasterInvalidData = [
+  "Error",
+  {
+    description: "Cannot process data from invalid report"
+  }
+];
+
 
 const TableCell = ({ icon, label }) => (
   <div className="tableCell">
@@ -20,153 +31,95 @@ const TableCell = ({ icon, label }) => (
 
 const columns = [
   {
-    title: 'Region',
-    dataIndex: 'region',
+    title: 'Application',
+    dataIndex: 'application',
     key: 'c1',
     render: (arr) => <TableCell icon={arr[0]} label={arr[1]} />,
   },
   {
-    title: 'Upper limit',
-    dataIndex: 'upperLimit',
+    title: 'Block',
+    dataIndex: 'block',
     key: 'c2',
-    render: (arr) => <TableCell icon={arr[0]} label={arr[1]} />,
-  },
-  {
-    title: 'Lower limit',
-    dataIndex: 'lowerLimit',
-    key: 'c3',
     render: (arr) => <TableCell icon={arr[0]} label={arr[1]} />,
   },
   {
     title: 'Measurement',
     dataIndex: 'measurement',
-    key: 'c4',
+    key: 'c3',
     render: (arr) => <TableCell icon={arr[0]} label={arr[1]} />,
   },
   {
     title: 'Timestamp',
     dataIndex: 'timestamp',
-    key: 'c5',
+    key: 'c4',
     render: (arr) => <TableCell icon={arr[0]} label={arr[1]} />,
   },
   {
     title: 'Validation',
     dataIndex: 'validation',
-    key: 'c6',
+    key: 'c5',
     render: icon => <TableCell icon={icon} />,
   }
 ];
 
-const dataTest = [
+// Contains data in the format table columns need
+const fallback = [
   {
     key: '1',
-    region: [<Product />, "ROI 1"],
-    upperLimit: [<PeakHigh />, 105],
-    lowerLimit: [<PeakLow />, 94],
-    measurement: [<Thickness />, 94],
-    timestamp: [<Runtime />, "04/11/2021 10:15"],
-    validation: <img src={statusOk} alt="status-ok" />,
-  },
-  {
-    key: '2',
-    region: [<Product />, "ROI 2"],
-    upperLimit: [<PeakHigh />, 97],
-    lowerLimit: [<PeakLow />, 83],
-    measurement: [<Thickness />, 94],
-    timestamp: [<Runtime />, "05/11/2021 10:15"],
-    validation: <img src={statusOk} alt="status-ok" />,
-  },
-  {
-    key: '3',
-    region: [<Product />, "ROI 3"],
-    upperLimit: [<PeakHigh />, 103],
-    lowerLimit: [<PeakLow />, 91],
-    measurement: [<Thickness />, 103],
-    timestamp: [<Runtime />, "06/11/2021 10:15"],
-    validation: <img src={statusWarning} alt="status-warning" />,
-  },
-  {
-    key: '4',
-    region: [<Product />, "ROI 4"],
-    upperLimit: [<PeakHigh />, 87],
-    lowerLimit: [<PeakLow />, 79],
-    measurement: [<Thickness />, 84],
-    timestamp: [<Runtime />, "07/11/2021 10:15"],
-    validation: <img src={statusOk} alt="status-ok" />,
-  },
-  {
-    key: '5',
-    region: [<Product />, "ROI 5"],
-    upperLimit: [<PeakHigh />, 99],
-    lowerLimit: [<PeakLow />, 83],
-    measurement: [<Thickness />, 97],
-    timestamp: [<Runtime />, "08/11/2021 10:15"],
-    validation: <img src={statusOk} alt="status-ok" />,
-  },
-  {
-    key: '6',
-    region: [<Product />, "ROI 6"],
-    upperLimit: [<PeakHigh />, 102],
-    lowerLimit: [<PeakLow />, 91],
-    measurement: [<Thickness />, 115],
-    timestamp: [<Runtime />, "09/11/2021 10:15"],
+    application: [<Application />, "Error"],
+    block: [<Product />, "Error"],
+    measurement: [<Thickness />, -1],
+    timestamp: [<Runtime />, "Error"],
     validation: <img src={statusAlert} alt="status-alert" />,
-  },
-  {
-    key: '7',
-    region: [<Product />, "ROI 7"],
-    upperLimit: [<PeakHigh />, 96],
-    lowerLimit: [<PeakLow />, 83],
-    measurement: [<Thickness />, 89],
-    timestamp: [<Runtime />, "10/11/2021 10:15"],
-    validation: <img src={statusOk} alt="status-ok" />,
-  },
-  {
-    key: '8',
-    region: [<Product />, "ROI 8"],
-    upperLimit: [<PeakHigh />, 105],
-    lowerLimit: [<PeakLow />, 94],
-    measurement: [<Thickness />, 94],
-    timestamp: [<Runtime />, "11/11/2021 10:15"],
-    validation: <img src={statusOk} alt="status-ok" />,
-  },
-  {
-    key: '9',
-    region: [<Product />, "ROI 9"],
-    upperLimit: [<PeakHigh />, 105],
-    lowerLimit: [<PeakLow />, 94],
-    measurement: [<Thickness />, 94],
-    timestamp: [<Runtime />, "12/11/2021 10:15"],
-    validation: <img src={statusOk} alt="status-ok" />,
-  },
-  {
-    key: '10',
-    region: [<Product />, "ROI 10"],
-    upperLimit: [<PeakHigh />, 105],
-    lowerLimit: [<PeakLow />, 974],
-    measurement: [<Thickness />, 94],
-    timestamp: [<Runtime />, "13/11/2021 10:15"],
-    validation: <img src={statusOk} alt="status-ok" />,
-  },
+  }
 ];
 
-const processDataFromReport = (report) => {
+const processDataFromReport = (report, threshold) => {
   console.log("[processDataFromReport]");
-
 
   if (_.isEmpty(report)) {
     return;
   }
 
   const reportAsObject = JSON.parse(report);
-  console.dir(reportAsObject);
+  let dataForTable;
 
+  // The report tuples might have eventually values like "-.-". Remove this.
+  
 
-  return dataTest;
+  try {
+    const filteredTuples = reportAsObject.tuples.filter(el => el[6] !== "-.-");
+    dataForTable = filteredTuples.map((el, index) => {
+      // Validation
+      const measurement = el[6];
+      const { lowerLimit, upperLimit } = threshold;
+      let validationIcon = <img src={statusAlert} alt="status-alert" />;
+
+      if ((measurement >= lowerLimit) && (measurement <= upperLimit)) {
+        validationIcon = <img src={statusOk} alt="status-ok" />;
+      }
+
+      return {
+        key: index,
+        application: [<Application />, el[1]],
+        block: [<Product />, el[3]],
+        measurement: [<Thickness />, measurement],
+        timestamp: [<Runtime />, el[5]],
+        validation: validationIcon,
+      }
+    });
+
+  } catch (error) {
+    toaster.danger(...propsToasterInvalidData);
+    return fallback;
+  }
+
+  return dataForTable;
 }
 
-const CustomTable = ({ report }) => {
-  const dataTable = processDataFromReport(report);
+
+const CustomTable = ({ report, threshold }) => {
+  const dataTable = processDataFromReport(report, threshold);
 
   return (
     <Table
